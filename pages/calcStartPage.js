8a4befe6ab829a2b3156c45f9185017b4c182ccc
GertrudaVal <gertruda.val@outlook.com>
2021-08-10 22:00:18 +0300

    Homework updates.

--- pages/calcStartPage.js
+++ pages/calcStartPage.js
@@ -9,8 +9,25 @@ exports.CalcStartPage = class CalcStartPage {
     await this.page.goto("https://testsheepnz.github.io/BasicCalculator");
   }
 
-  async fillFields(numberOne, numberTwo) {
+  async calculate(numberOne, numberTwo) {
     await this.page.fill("#number1Field", numberOne.toString());
     await this.page.fill("#number2Field", numberTwo.toString());
+    await this.page.click("#calculateButton");
+  }
+
+  async assertFieldExistsAndIsEnabled(fieldId) {
+    const visible = await this.page.isVisible(fieldId);
+    expect(visible).toBeTruthy();
+
+    const enabled = await this.page.isEnabled(fieldId);
+    expect(enabled).toBeTruthy();
+  }
+
+  async getAnswer() {
+    return await this.page.inputValue("#numberAnswerField");
+  }
+
+  async selectBuild(build) {
+    await this.page.selectOption("#selectOperationDropdown", `${build}`);
   }
 };
